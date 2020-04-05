00fd91d6e5139c60c2f34679a0418389152a2497
javcela10 <javiercela1007@gmail.com>
2020-04-05 18:41:01 +0200

    Crear clase

--- src/COVID-19_Scraper.py
+++ src/COVID-19_Scraper.py
@@ -1,1 +1,8 @@
 from bs4 import BeautifulSoup
+
+class COVID19Scrapper():
+
+    def __init__(self):
+
+        self.urlWorldometers = 'https://www.worldometers.info/coronavirus/#countries'
+        self.urlPIB = 'https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_por_PIB_(nominal)'
